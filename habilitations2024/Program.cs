3c99f534d6ff2c23cd81119d9d2cb27b555f1fba
Margaux-Lacheze <75217835+Margaux-Lacheze@users.noreply.github>
2025-01-24 14:03:51 +0100

    Ajoutez des fichiers projet.

---
+++ habilitations2024/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using habilitations2024.view;
+
+namespace habilitations2024
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmHabilitations());
+        }
+    }
+}
---
+++ habilitations2024/view/FrmHabilitations.Designer.cs
@@ -0,0 +1,25 @@
+﻿namespace habilitations2024.view
+{
+    partial class FrmHabilitations
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
+
